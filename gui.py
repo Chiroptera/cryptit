df23f165633c8fd30344780bf29dafa4f128b788
Diogo Silva <dasilva@academiafa.edu.pt>
2016-06-17 17:28:01 +0100

    button

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
 from kivy.config import Config
@@ -17,8 +18,9 @@ class PasswordScreen(GridLayout):
         super(PasswordScreen, self).__init__(**kwargs)
         self.cols = 1
         self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
+        self.password = TextInput(text='password',password=True, multiline=False)
         self.add_widget(self.password)
+        self.add_widget(Button(text='decrypt'))
 
 
 class MyApp(App):
